fdac90db5be86b5ba10ca286d376049810cb95ac
lsl <273974052@qq.com>
2019-06-27 10:01:43 +0800

    vue  功能

--- src/router/index.js
+++ src/router/index.js
@@ -28,7 +28,6 @@ export default new Router({
                 {
                     path: '/dashboard',
                     component: () => import('@/components/views/Dashboard.vue'),
-
                     meta: { title: '系统首页' }
                 },
                 //     {
