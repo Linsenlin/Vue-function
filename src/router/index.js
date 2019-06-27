e6fc3d3622b32d910a63e59151b5cb1556e2c4f4
lsl <273974052@qq.com>
2019-06-27 09:58:55 +0800

    vue  功能

--- src/router/index.js
+++ src/router/index.js
@@ -23,7 +23,6 @@ export default new Router({
         {
             path: '/',
             component: () => import('@/components/common/Home.vue'),
-            component: resolve => require(['../components/common/Home.vue'], resolve),
             meta: { title: '自述文件' },
             children: [
                 {
@@ -32,84 +31,84 @@ export default new Router({
 
                     meta: { title: '系统首页' }
                 },
-            //     {
-            //         path: '/icon',
-            //         component: () => import('@/components/views/Icon.vue'),
-            //         meta: { title: '自定义图标' }
-            //     },
-            //     {
-            //         path: '/table',
-            //         component: () => import('@/components/views/BaseTable.vue'),
-            //         meta: { title: '基础表格' }
-            //     },
-            //     {
-            //         path: '/tabs',
-            //         component: () => import('@/components/views/Tabs.vue'),
-            //         meta: { title: 'tab选项卡' }
-            //     },
-            //     {
-            //         path: '/form',
-            //         component: () => import('@/components/views/BaseForm.vue'),
-            //         meta: { title: '基本表单' }
-            //     },
-            //     {
-            //         // 富文本编辑器组件
-            //         path: '/editor',
-            //         component: () => import('@/components/views/VueEditor.vue'),
-            //         meta: { title: '富文本编辑器' }
-            //     },
-            //     {
-            //         // markdown组件
-            //         path: '/markdown',
-            //         component: () => import('@/components/views/Markdown.vue'),
-            //         meta: { title: 'markdown编辑器' }
-            //     },
-            //     {
-            //         // 图片上传组件
-            //         path: '/upload',
-            //         component: () => import('@/components/views/Upload.vue'),
-            //         meta: { title: '文件上传' }
-            //     },
-            //     {
-            //         // vue-schart组件
-            //         path: '/charts',
-            //         component: () => import('@/components/views/BaseCharts.vue'),
-            //         meta: { title: 'schart图表' }
-            //     },
-            //     {
-            //         // 拖拽列表组件
-            //         path: '/drag',
-            //         component: () => import('@/components/views/DragList.vue'),
-            //         meta: { title: '拖拽列表' }
-            //     },
-            //     {
-            //         // 拖拽Dialog组件
-            //         path: '/dialog',
-            //         component: () => import('@/components/views/DragDialog.vue'),
-            //         meta: { title: '拖拽弹框' }
-            //     },
-            //     {
-            //         // 国际化组件
-            //         path: '/i18n',
-            //         component: () => import('@/components/views/I18n.vue'),
-            //         meta: { title: '国际化' }
-            //     },
-            //     {
-            //         // 权限页面
-            //         path: '/permission',
-            //         component: () => import('@/components/views/Permission.vue'),
-            //         meta: { title: '权限测试', permission: true }
-            //     },
-            //     {
-            //         path: '/404',
-            //         component: () => import('@/components/views/404.vue'),
-            //         meta: { title: '404' }
-            //     },
-            //     {
-            //         path: '/403',
-            //         component: () => import('@/components/views/403.vue'),
-            //         meta: { title: '403' }
-            //     }
+                //     {
+                //         path: '/icon',
+                //         component: () => import('@/components/views/Icon.vue'),
+                //         meta: { title: '自定义图标' }
+                //     },
+                //     {
+                //         path: '/table',
+                //         component: () => import('@/components/views/BaseTable.vue'),
+                //         meta: { title: '基础表格' }
+                //     },
+                //     {
+                //         path: '/tabs',
+                //         component: () => import('@/components/views/Tabs.vue'),
+                //         meta: { title: 'tab选项卡' }
+                //     },
+                //     {
+                //         path: '/form',
+                //         component: () => import('@/components/views/BaseForm.vue'),
+                //         meta: { title: '基本表单' }
+                //     },
+                //     {
+                //         // 富文本编辑器组件
+                //         path: '/editor',
+                //         component: () => import('@/components/views/VueEditor.vue'),
+                //         meta: { title: '富文本编辑器' }
+                //     },
+                //     {
+                //         // markdown组件
+                //         path: '/markdown',
+                //         component: () => import('@/components/views/Markdown.vue'),
+                //         meta: { title: 'markdown编辑器' }
+                //     },
+                //     {
+                //         // 图片上传组件
+                //         path: '/upload',
+                //         component: () => import('@/components/views/Upload.vue'),
+                //         meta: { title: '文件上传' }
+                //     },
+                //     {
+                //         // vue-schart组件
+                //         path: '/charts',
+                //         component: () => import('@/components/views/BaseCharts.vue'),
+                //         meta: { title: 'schart图表' }
+                //     },
+                //     {
+                //         // 拖拽列表组件
+                //         path: '/drag',
+                //         component: () => import('@/components/views/DragList.vue'),
+                //         meta: { title: '拖拽列表' }
+                //     },
+                //     {
+                //         // 拖拽Dialog组件
+                //         path: '/dialog',
+                //         component: () => import('@/components/views/DragDialog.vue'),
+                //         meta: { title: '拖拽弹框' }
+                //     },
+                //     {
+                //         // 国际化组件
+                //         path: '/i18n',
+                //         component: () => import('@/components/views/I18n.vue'),
+                //         meta: { title: '国际化' }
+                //     },
+                //     {
+                //         // 权限页面
+                //         path: '/permission',
+                //         component: () => import('@/components/views/Permission.vue'),
+                //         meta: { title: '权限测试', permission: true }
+                //     },
+                //     {
+                //         path: '/404',
+                //         component: () => import('@/components/views/404.vue'),
+                //         meta: { title: '404' }
+                //     },
+                //     {
+                //         path: '/403',
+                //         component: () => import('@/components/views/403.vue'),
+                //         meta: { title: '403' }
+                //     }
             ]
         },
 
